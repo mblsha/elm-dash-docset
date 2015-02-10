a53133fd7e16092de1bb9919e09709951903042c
Michail Pishchagin <mblsha@yandex-team.ru>
2015-02-10 22:34:24 +0300

    Generate index.json

--- gen_docset.rb
+++ gen_docset.rb
@@ -12,6 +12,7 @@ require 'rubysh'
 ROOT = File.absolute_path(File.join(File.dirname(__FILE__), 'docroot'))
 package_template = ERB.new(File.read(File.join(ROOT, 'templates', 'package.erb.html')))
 module_template = ERB.new(File.read(File.join(ROOT, 'templates', 'module.erb.html')))
+index_array = []
 
 def build_elm_files
   Dir.chdir(File.join(ROOT, '..', 'package.elm-lang.org')) do
@@ -38,6 +39,12 @@ all_packages_dict.each do |package|
   package_path = File.join(ROOT, 'packages', name, version)
   FileUtils::mkdir_p package_path
 
+  index_array += [{
+    'type' => 'Package',
+    'name' => "#{name}/#{version}",
+    'path' => "#{package_path}/index.html"
+  }]
+
   documentation_path = File.join(package_path, 'documentation.json')
   # next if File.exist? documentation_path
 
@@ -56,6 +63,12 @@ all_packages_dict.each do |package|
   documentation_json.each do |module_dict|
     module_name = module_dict['name'] #
     module_path = File.join(package_path, escape_module(module_name))
+    index_array += [{
+      'type' => 'Module',
+      'name' => module_name,
+      'path' => "#{module_path}/index.html"
+    }]
+
     FileUtils::mkdir_p(module_path)
     File.open(File.join(module_path, 'index.html'), 'wb') do |f|
       erb_user, erb_name = name.split('/')
@@ -66,4 +79,8 @@ all_packages_dict.each do |package|
   end
 
   break
+end
+
+File.open(File.join(ROOT, 'index.json'), 'wb') do |f|
+  f.write JSON.pretty_generate(index_array)
 end
